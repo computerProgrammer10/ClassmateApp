cfb752d4b3b8abe2e734696f9d4533963130e186
Daniel <daniel.huseby26@d155.org>
2024-09-30 08:16:36 -0500

    Initial Commit

---
+++ ClassmateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassmateApp
+//
+//  Created by DANIEL HUSEBY on 9/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ClassmateAppTests/ClassmateAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClassmateAppTests.swift
+//  ClassmateAppTests
+//
+//  Created by DANIEL HUSEBY on 9/30/24.
+//
+
+import Testing
+@testable import ClassmateApp
+
+struct ClassmateAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
